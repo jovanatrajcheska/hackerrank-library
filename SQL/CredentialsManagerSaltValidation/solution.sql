0982358369d8981128f52eb64347bc94b638af6e
jovanatrajcheska <trajceskaj55@gmail.com>
2024-04-23 19:33:11 +0200

    Updated solution

--- SQL/CredentialsManagerSaltValidation/solution.sql
+++ SQL/CredentialsManagerSaltValidation/solution.sql
@@ -1,14 +1,5 @@
-SELECT
-  mac,
-  COUNT( * ) AS salts
-  FROM
-    encryptions e
-      LEFT JOIN accounts a
-        ON e.account_id = a.id
-  WHERE
-    LENGTH( salt ) < 8 AND
-    is_active
-  GROUP BY
-    mac
-  ORDER BY
-    mac
+SELECT mac, COUNT( * ) AS salts
+FROM encryptions e
+LEFT JOIN accounts a ON e.account_id = a.id
+WHERE LENGTH( salt ) < 8 AND is_active
+GROUP BY mac ORDER BY mac
